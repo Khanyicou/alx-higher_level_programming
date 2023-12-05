32c7392121167542251e3c3ebf15f9a179c0e610
Khanyicou <preciouskhanyisa1@gmail.com>
2023-12-04 19:55:41 -0800

    updte

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Establish a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """shows base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
