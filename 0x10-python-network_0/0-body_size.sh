b4f61dbfebda86403c4a982a7395cf90cfd92a68
Khanyicou <preciouskhanyisa1@gmail.com>
2024-02-29 22:21:07 -0800

    ou

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Get the byte size of the HTTP response header for a given URL.
+curl -Is "$1" | grep Content-Length | cut -f2 -d' '
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#script Send a DELETE request to a given URL and display the response body.
+curl -sX DELETE "$1"
