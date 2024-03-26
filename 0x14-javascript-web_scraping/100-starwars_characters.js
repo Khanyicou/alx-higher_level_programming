2746e537dc71e6cba47cbb7075286cdcab2538c9
Khanyicou <preciouskhanyisa1@gmail.com>
2024-03-26 13:47:54 -0700

    ou

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,9 +4,7 @@ const req = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/';
 req.get(url + id, function (error, res, body) {
-  if (error)
-
-	{
+  if (error) {
     console.log(error);
 
   }
